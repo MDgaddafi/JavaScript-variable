22553bd4b6c6e71cb513cf06b567fd4143bc557b
Babua Hossain <babuahossain@Babuas-Mac-Studio.local>
2025-04-06 12:39:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const weight = 38;
+const price = 760;
+const year = 1860;
+const age1 = 14;
+
+
+const age = 1996;
+const siblings = 6;
+const students = 73;
+const marks = 60;
+const rooms = 4;
+const hours = 8;
+const total = 56;
+
+const booksToRead = 50;
+const wakeUpTime = 11;
+const wastedTime = 6;
+const borrow = 500;
+const goals = 7;
+const pillow = 1200;
+
+
+ 
+console.log(total);
+console.log(age);
+console.log(booksToRead);
+console.log(wakeUpTime);
+console.log("wastedTime " + wastedTime + " hour");
+console.log(borrow + " taka");
+console.log(goals + " Goals");
+console.log(pillow + " Pillow");
+
+
+
