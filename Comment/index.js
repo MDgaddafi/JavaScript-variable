017c9eb77a0583d33135f2861fad8c6ebcfab420
Babua Hossain <babuahossain@Babuas-Mac-Studio.local>
2025-05-31 10:54:34 +0600

    update

---
+++ Comment/index.js
@@ -0,0 +1,17 @@
+const number = '20';
+const newNumber = 10;
+const totalNumber = parseInt(number) + newNumber;
+
+
+const twoNumber = 3.12456;
+const boy = "PramikBoy";
+
+const numbeOne = 3.456;
+const numberTwo = 2.789;
+const allNumber = numbeOne + numberTwo;
+
+
+console.log(totalNumber) 
+console.log(parseInt(twoNumber));   
+console.log(parseInt(boy));   
+console.log(allNumber.toFixed(2))                                                    
---
+++ Variable/index.js
@@ -0,0 +1,64 @@
+const weight = 38;
+const price = 760;
+const year = 1860;
+const age1 = 14;
+
+
+const age = 1996;
+const siblings = 6;
+const students = 73;
+const marks = 60;
+const rooms = 4;
+const hours = 8;
+const total = 56;
+
+const booksToRead = 50;
+const wakeUpTime = 11;
+const wastedTime = 6;
+const borrow = 500;
+const goals = 7;
+const pillow = 1200;
+
+const chotVai = 10;
+const me = 15;
+const totalAge = chotVai <= me;
+
+const jambura = 20;
+const kotbel = 40;
+const priceBD = jambura < kotbel;
+
+const me2 = 15;
+const firend = 20
+const totalbisqute = me2 < firend;
+
+const realmardi = 2;
+const barsalona = 3;
+const totalGol = realmardi < barsalona;
+
+const name1 = "rohim";
+const name2 = "koraim";
+const isname = name1 === name2;
+
+const meBookRead = true;
+const firendBookRead = false;
+const isBookRead = meBookRead == firendBookRead;
+
+
+
+console.log(isBookRead);
+console.log(isname);
+console.log(totalGol);
+console.log(totalbisqute);
+console.log(priceBD);
+console.log(totalAge);
+console.log(total);
+console.log(age);
+console.log(booksToRead);
+console.log(wakeUpTime);
+console.log("wastedTime " + wastedTime + " hour");
+console.log(borrow + " taka");
+console.log(goals + " Goals");
+console.log(pillow + " Pillow");
+
+
+
